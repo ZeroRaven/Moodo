a0f5746a053bf7f7d17a2b061772cc593104083f
Munsat Rukaya <munsatrukaya@192-168-1-103.tpgi.com.au>
2023-06-10 10:00:26 +1000

    deploys

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,5 +1,14 @@
-const NotFound = () => {
-    return <p>BLAAAAAAH</p>
-}
+import { Container, Image } from "@chakra-ui/react";
 
-export default NotFound
+const NotFound = () => {
+  return (
+    <Container>
+      <Image
+        src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=740&t=st=1686353726~exp=1686354326~hmac=28d331651d9879f310f264e535aa692c5fd666d24e96f860ed775b404f9d9192"
+        alt="page not found error"
+      />
+    </Container>
+  );
+};
+
+export default NotFound;
